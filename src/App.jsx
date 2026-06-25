ba4ec6bb6d68b238bd4192ac33b4b5d3969ea0c3
debashis <debashisbaral469@gmail.com>
2026-06-25 14:21:32 +0530

    Changes made

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Boom Boom Vite</h1>
+      <h1>Now we are testing auto deployment</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
